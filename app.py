e4eefc357b7bb6f7ef14557bf0d875574d6772e7
Guy Lagrand <guy@scrmworks.nl>
2020-02-02 16:11:39 +0100

    Comment procress csv

--- app.py
+++ app.py
@@ -77,18 +77,18 @@ def importCSV():
     if request.method == "POST":
         # Read CSV file
         print('>>>>> POST CSV <<<<<')
-        if not request.files["fileX"]:
-            flash('Missing import file, please select a CSV-file from your computer', 'danger')
-            return render_template("import.html") 
-        try:
-            data = pd.read_csv(request.files["fileX"], sep=";")
-            print('>>>>> STORE ROWS IN DB<<<<<')
-            processImport(data)
-            print('>>>>> DONE STORING ROWS IN DB<<<<<')
-            flash('Transactions are stored, you can now process them', 'success')
-        except Exception as e:
-            print(">>>>> exception <<<<<", e)
-            flash('Invalid file or a database error, send an email to slackbyte8@gmail.com', 'danger')
+        # if not request.files["fileX"]:
+        #     flash('Missing import file, please select a CSV-file from your computer', 'danger')
+        #     return render_template("import.html") 
+        # try:
+        #     data = pd.read_csv(request.files["fileX"], sep=";")
+        #     print('>>>>> STORE ROWS IN DB<<<<<')
+        #     processImport(data)
+        #     print('>>>>> DONE STORING ROWS IN DB<<<<<')
+        #     flash('Transactions are stored, you can now process them', 'success')
+        # except Exception as e:
+        #     print(">>>>> exception <<<<<", e)
+        #     flash('Invalid file or a database error, send an email to slackbyte8@gmail.com', 'danger')
         return redirect("/")
     else:
         print(">>> /GET <<<")
